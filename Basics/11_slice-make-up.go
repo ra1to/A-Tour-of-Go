27e77ee7993ab426cf79410431ccd27a572ee27f
ra1to <laitoxinyuan@gmail.com>
2026-06-05 20:52:21 +0900

    Basic

--- Basics/11_slice-make-up.go
+++ Basics/11_slice-make-up.go
@@ -1,17 +1,29 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	n := make([]int, 3, 5)
-	fmt.Println("len=%d cap=%d value=%v", len(n), cap(n), n)
-	n = append(n, 0, 0)
-	fmt.Println("len=%d cap=%d value=%v", len(n), cap(n), n)
-	n = append(n, 1, 2, 3, 4, 5)
-	fmt.Println("len=%d cap=%d value=%v", len(n), cap(n), n)
+// func main() {
+// 	// n := make([]int, 3, 5)
+// 	// fmt.Println("len=%d cap=%d value=%v", len(n), cap(n), n)
+// 	// n = append(n, 0, 0)
+// 	// fmt.Println("len=%d cap=%d value=%v", len(n), cap(n), n)
+// 	// n = append(n, 1, 2, 3, 4, 5)
+// 	// fmt.Println("len=%d cap=%d value=%v", len(n), cap(n), n)
 
-	a := make([]int, 3)
-	fmt.Println("len=%d cap=%d value=%v", len(a), cap(a), a)
-	b := make([]int, 0)
-	fmt.Println("len=%d cap=%d value=%v", len(b), cap(b), b)
-}
+// 	// a := make([]int, 3)
+// 	// fmt.Println("len=%d cap=%d value=%v", len(a), cap(a), a)
+
+// 	// b := make([]int, 0)
+// 	// var c []int
+// 	// fmt.Println("len=%d cap=%d value=%v", len(b), cap(b), b)
+// 	// fmt.Println("len=%d cap=%d value=%v", len(c), cap(c), c)
+
+// 	var c []int
+// 	// c = make([]int, 5)
+// 	c = make([]int, 0, 5)
+// 	for i := 0; i < 5; i++ {
+// 		c = append(c, i)
+// 		fmt.Println(c)
+// 	}
+// 	fmt.Println(c)
+// }
---
+++ Basics/12_map.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+
+// func main() {
+//	m := map[string]int{"apple": 100, "banana": 200}
+// 	fmt.Println(m)
+// 	fmt.Println(m["apple"])
+// 	m["banana"] = 300
+// 	fmt.Println(m)
+
+// 	 fmt.Println(m["nothing"])
+// 	 v, ok := m["nothing"]
+// 	 fmt.Println(v, ok)
+
+// m2 := make(map[string]int)
+// 	 m2["pc"] = 5000
+//  fmt.Println(m2)
+
+// 	var m3 map[string]int
+// 	m3["pc"] = 5000
+// 	fmt.Println(m3)
+
+// 	var s []int
+// 	if s == nil {
+// 		fmt.Println(s)
+// 	}
+// }
