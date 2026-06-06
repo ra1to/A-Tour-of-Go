0cca636a21e5249b7c3c4d983ea69b2d3914e7ac
ra1to <laitoxinyuan@gmail.com>
2026-06-06 23:54:18 +0900

    Basic

--- Basics/13_bite.go
+++ Basics/13_bite.go
@@ -1,4 +1,13 @@
 package main
 
-func main() {
-}
+// import "fmt"
+
+// func main() {
+// 	b := []byte{72, 73}
+// 	fmt.Println(b)
+// 	fmt.Println(string(b))
+
+// 	c := []byte("HI")
+// 	fmt.Println(c)
+// 	fmt.Println(string(c))
+// }
