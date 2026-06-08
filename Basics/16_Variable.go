a0ab110f737d175364df61c5fcbb0d30eafad14f
ra1to <laitoxinyuan@gmail.com>
2026-06-08 18:05:35 +0900

    Basic

---
+++ Basics/16_Variable.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// func foo(params ...int) {
+// 	for _, param := range params {
+// 		fmt.Println(param)
+// 	}
+// }
+
+// func main() {
+// 	foo(10, 20)
+// 	foo(10, 20, 30)
+// }
